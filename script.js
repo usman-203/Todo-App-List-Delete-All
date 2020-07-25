c0a6923d8ef060f2f393f364b724e277fe7428c3
usman-203 <62180188+usman-203@users.noreply.github.com>
2020-07-25 07:21:09 +0500

    commit2

---
+++ script.js
@@ -0,0 +1,12 @@
+function todoList(){
+    
+    var item = document.getElementById("todoInput").value
+    var text = document.createTextNode(item)
+    var newItem = document.createElement("li")
+    newItem.appendChild(text)
+    document.getElementById("todoList").appendChild(newItem)
+}
+function deleteTodeList(){
+    document.getElementById("todoList").remove("li")
+    
+}
